27d8d29c2e3da815ec541b72e48615dff265a31d
DYaroslav <34601776+MeetEatMeat@users.noreply.github.com>
2024-07-29 19:21:47 +0500

    GraphBuilder started

---
+++ src/blockchain/libs/GraphBuilder.ts
@@ -0,0 +1,35 @@
+import * as graphlib from 'graphlib';
+import { Transaction } from '../dto/transaction.dto';
+
+interface TransactionNode {
+  address: string;
+  transactions: Transaction[];
+}
+
+class GraphBuilder {
+  private graph: graphlib.Graph;
+
+  constructor() {
+    this.graph = new graphlib.Graph({ directed: false });
+  }
+
+  addTransaction(transaction: Transaction) {
+    if (!this.graph.hasNode(transaction.from)) {
+      this.graph.setNode(transaction.from);
+    }
+
+    if (!this.graph.hasNode(transaction.to)) {
+      this.graph.setNode(transaction.to);
+    }
+
+    this.graph.setEdge(transaction.from, transaction.to);
+  }
+
+  buildGraph(transactions: Transaction[]) {
+    transactions.forEach(transaction => this.addTransaction(transaction));
+  }
+
+  getGraph(): graphlib.Graph {
+    return this.graph;
+  }
+}
